56ceebb1c0577b3f9f67a6a328f419f3c21cd6c9
Raeid Saqur <raeidsaqur@gmail.com>
2017-02-14 19:21:50 -0500

    Refactored code

--- Animation.playground/Pages/Animations.xcplaygroundpage/Contents.swift
+++ Animation.playground/Pages/Animations.xcplaygroundpage/Contents.swift
@@ -5,6 +5,8 @@ import UIKit
 import XCPlayground
 
 
+
+//Define a screen (typical iPhone device size)
 let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
 XCPShowView(identifier: "Container View", view: containerView)
 
@@ -20,26 +22,39 @@ containerView.addSubview(circle);
 let rectangle = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0))
 rectangle.center = containerView.center
 rectangle.layer.cornerRadius = 5.0
-
 rectangle.backgroundColor = UIColor.white
 
 containerView.addSubview(rectangle)
 
 //Mark: - UIView Animate Code
 
+
 UIView.animate(withDuration: 2.0, animations: { () -> Void in
     
     let endingColor = UIColor.orange
-    circle.backgroundColor = endingColor
-    
     let scaleTransform = CGAffineTransform(scaleX: 5.0, y: 5.0)
-    
-    circle.transform = scaleTransform
-    
     let rotationTransform = CGAffineTransform(rotationAngle: 3.14)
     
+    circle.backgroundColor = endingColor
+    circle.transform = scaleTransform
     rectangle.transform = rotationTransform
 })
+
+//UIView.animate(withDuration: 1.0, delay: 0, options:[.repeat, .autoreverse], animations: { () -> Void in
+//
+//    //Step 2: Define end state
+//    let endingColor = UIColor.orange
+//    let scaleTransform = CGAffineTransform(scaleX: 5.0, y: 5.0)
+//    let rotationTransform = CGAffineTransform(rotationAngle: 3.14)
+//
+//    circle.backgroundColor = endingColor
+//    circle.transform = scaleTransform
+//    rectangle.transform = rotationTransform
+//
+//}) { (success) -> Void in
+//    //Success: Revert back
+//   print("Success")
+//}
 
 
 
